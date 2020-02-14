7e73d1be44fb7d68231c5cd408bf8ca9896be204
Mauricio Amorim <mjvamorim@gmail.com>
2020-02-14 18:31:59 -0300

    Company mudando para Empresa

--- src/config/crud.php
+++ src/config/crud.php
@@ -3,6 +3,5 @@
 return [
     'doctor' => 'App\Models\Doctor',
     'user' => 'App\User',
-    'company' => 'Amorim\Tenant\Models\Company',
-    'example' => 'Amorim\Crud\Models\Company',
+    'empresa' => 'Amorim\Tenant\Models\Empresa',
 ];
