c7765bbf6c3c70c9bbc24e50743cd028702eb743
Mauricio Amorim <mjvamorim@gmail.com>
2020-11-05 10:09:46 -0300

    Nomeou a rota crud

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,7 @@ Route::group(['middleware' => ['web','auth','tenant']], function () {
     //     return view('crud::index');
     // });
     Route::get('crud/{model}', 'Amorim\Crud\Controllers\CrudController@index');
-    Route::get('crud/getdata/{model}', 'Amorim\Crud\Controllers\CrudController@getData');
+    Route::get('crud/getdata/{model}', 'Amorim\Crud\Controllers\CrudController@getData')->name('crud/getdata');;
     Route::get('crud/fetchdata/{model}', 'Amorim\Crud\Controllers\CrudController@fetchData');
     Route::post('crud/postdata/{model}', 'Amorim\Crud\Controllers\CrudController@postData');
 
